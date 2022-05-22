da25a80310f09de798458cc72338723ccac563e6
Pavitra Raut <pareshraut50@gmail.com>
2022-05-22 14:17:31 +0530

    feat: Connected with mock (db.json) json-server

--- src/App.js
+++ src/App.js
@@ -1,49 +1,79 @@
 import Header from "./components/Header";
 import Tasks from './components/Tasks'
 import AddTask from "./components/AddTask";
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
 
+  const baseUrl = "http://localhost:5000/"
+
   const [showNewTaskForm, setShowNewTaskForm] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Learn React JS (DOM)',
-        day: 'May 20th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Learn React Native',
-        day: 'May 21th at 12:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Start with OLA app with React Native',
-        day: 'May 24th at 10:30am',
-        reminder: true
-    },
-  ])
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const data = await fetchTasks()
+      setTasks(data)
+    }
+    getTasks()
+  }, [])
+
+  // Fetch a single task
+  const fetchTask = async (id) => {
+    const urlGetTasks = `${baseUrl}tasks/${id}`
+    const request = await fetch(urlGetTasks)
+    const data = await request.json()
+    return data
+  }
+
+  // Fetch tasks
+  const fetchTasks = async () => {
+    const urlGetTasks = `${baseUrl}tasks`
+    const request = await fetch(urlGetTasks)
+    const data = await request.json()
+    return data
+  }
 
   // Add new task
-  const addNewTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = {id, ...task}
-    setTasks([newTask, ...tasks])
+  const addNewTask = async (task) => {
+    const urlAddTask = `${baseUrl}tasks`
+    const request = await fetch(urlAddTask, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await request.json()
+    setTasks([...tasks, data])
     setShowNewTaskForm(false)
   }
 
   // Delete task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    const urlDeleteTask = `${baseUrl}tasks/${id}`
+    await fetch(urlDeleteTask, { method: 'DELETE' })
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const task = await fetchTask(id)
+    const updTask = {... task, reminder: !task.reminder}
+
+    const urlUpdateTask = `${baseUrl}tasks/${id}`
+    const request = await fetch(urlUpdateTask, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await request.json()
     setTasks(tasks.map((task) => task.id === id ? 
-      {...task, reminder: !task.reminder} : task))
+      {...task, reminder: data.reminder} : task))
   }
 
   return (
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,9 +2,10 @@ import PropTypes from 'prop-types'
 import Task from './Task'
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
+    const reversedTasks = [].concat(tasks).reverse()
     return (
         <>
-            {tasks.map((task) => (
+            {reversedTasks.map((task) => (
                 <Task key={task.id} 
                     task={task} 
                     onDelete={onDelete} 
